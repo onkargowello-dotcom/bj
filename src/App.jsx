b17fe13bb169daf34aebf1d452085c6c5b07b274
onkargowello-dotcom <onkar.gowello@gmail.com>
2026-01-20 15:37:07 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
           {/* Backend connected via FormSubmit */}
           <form
             className="contact-form"
-            action="https://formsubmit.co/onkardate9@gmail.com"
+            action="https://formsubmit.co/el/yuzexi/onkardate9@gmail.com"
             method="POST"
           >
             <input type="text" name="name" placeholder="Your Name" required />
